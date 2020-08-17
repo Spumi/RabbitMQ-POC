12748789e977a7d075efdb04b68ff06a33d9e0fe
Krisztián <Krisztian@pometko.hu>
2020-08-17 15:36:56 +0200

    RabbitMq Send+Recv test

---
+++ rabbit-recv/CommandHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rabbit_recv
+{
+    class CommandHandler
+    {
+    }
+}
---
+++ rabbit/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace rabbit
+{
+    class Program
+    {
+        private static string getIp() {
+            IPHostEntry host;
+            string localIP = "?";
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIP = ip.ToString();
+                }
+            }
+            return localIP;
+        }
+
+        static void Main(string[] args)
+        {
+           
+
+            var factory = new ConnectionFactory() { HostName = "192.168.1.66" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Hello World! from: " + getIp();
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            //Console.ReadLine();
+        }
+    }
+}
