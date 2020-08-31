fea13528f3e0d71d2bf900d7531c0b9137abebf6
Krisztián <Krisztian@pometko.hu>
2020-08-31 13:24:52 +0200

    Added user model to login controller

---
+++ Login/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login.Model
+{
+    public class User
+    {
+        public User()
+        {
+            RegisterDate = DateTime.Now;
+        }
+
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public string Auth_token { get; set; }
+
+        public void GenerateAuthToken()
+        {
+            string toHash = Username + Password + Stopwatch.GetTimestamp().ToString();
+            Auth_token = toHash.GetHashCode().ToString();
+        }
+    }
+}
