1657cbdceb4129baa16d274eb3454d41d5999ce8
Krisztián <Krisztian@pometko.hu>
2020-08-31 09:40:44 +0200

    Added tcp websocket listener

--- ChatRoomServer/Program.cs
+++ ChatRoomServer/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
 
 namespace ChatRoomServer
 {
@@ -6,7 +8,14 @@ namespace ChatRoomServer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 80);
+
+            server.Start();
+            Console.WriteLine("Server started", Environment.NewLine);
+
+            TcpClient client = server.AcceptTcpClient();
+
+            Console.WriteLine("A client connected.");
         }
     }
 }
