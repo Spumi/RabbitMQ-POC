d4587eb8a3722b7cfaa7d158987cf1c23b6d26fb
Krisztián <Krisztian@pometko.hu>
2020-08-31 16:25:12 +0200

    Login: Added db support

---
+++ Login/UserContext.cs
@@ -0,0 +1,20 @@
+﻿using Login.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
