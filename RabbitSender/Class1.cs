8c7e57ac4eb8dec2052cfeed97e70c8409a1bf8f
Krisztián <Krisztian@pometko.hu>
2020-09-02 10:13:42 +0200

    Added message sender function to RabbitSend

--- RabbitSender/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace RabbitSender
-{
-    public class Class1
-    {
-    }
-}
--- RabbitSender/Send.cs
+++ RabbitSender/Send.cs
@@ -1,8 +1,31 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
 
 namespace RabbitSender
 {
-    public class Class1
+    public class Send
     {
+        public void SendMessage(string message, string routingKey)
+        {
+            var factory = new ConnectionFactory() { HostName = "192.168.1.66" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                                 routingKey: routingKey,
+                                                 basicProperties: null,
+                                                 body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+        }
     }
 }
